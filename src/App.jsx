4f11032911f5b7cdf2291bead6658ca0afdec07a
ZAINZESHAN <153626862+ZAINZESHAN@users.noreply.github.com>
2024-12-18 17:27:14 +0500

    Category Page Created Completely

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,36 @@ import ShopCategory from './Pages/ShopCategory'
 import Product from './Pages/Product'
 import Cart from './Pages/Cart'
 import LoginSignUp from './Pages/LoginSignUp'
+import Footer from './components/Footer/Footer'
+import men_banner from './assets/banner_mens.png'
+import women_banner from './assets/banner_women.png'
+import kid_banner from './assets/banner_kids.png'
 
 function App () {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Shop />}></Route>
-        <Route path='/mens' element={<ShopCategory />} category='mens' />
-        <Route path='/womens' element={<ShopCategory />} category='womens' />
-        <Route path='/kids' element={<ShopCategory />} category='kids' />
+        <Route path='/' element={<Shop />} />
+        <Route
+          path='/mens'
+          element={<ShopCategory banner={men_banner} category='men' />}
+        />
+        <Route
+          path='/womens'
+          element={<ShopCategory banner={women_banner} category='women' />}
+        />
+        <Route
+          path='/kids'
+          element={<ShopCategory banner={kid_banner} category='kid' />}
+        />
         <Route path='/product' element={<Product />}>
           <Route path=':productId' element={<Product />} />
         </Route>
-        <Route path='/cart' element={<Cart />}></Route>
-        <Route path='/login' element={<LoginSignUp/>}></Route>
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/login' element={<LoginSignUp />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
